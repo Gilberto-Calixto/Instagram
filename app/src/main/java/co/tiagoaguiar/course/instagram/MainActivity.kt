cbe8ea931a463676fbf67423b9dca6066a410838
Beto_Dev1# <gilbertocalixto2002@gmail.com>
2024-08-29 18:48:29 -0300

    LoadingButton: Criando Seus Próprios Componentes UI

--- app/src/main/java/co/tiagoaguiar/course/instagram/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/MainActivity.kt
@@ -1,19 +1,53 @@
 package co.tiagoaguiar.course.instagram
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
 import co.tiagoaguiar.course.instagram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    binding = ActivityMainBinding.inflate(layoutInflater)
-    setContentView(binding.root)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val email = binding.loginTextEmail
+        val password = binding.loginTextPassword
+
+        email.addTextChangedListener(watcher)
+        password.addTextChangedListener(watcher)
+
+        val btnEnter: LoadingButton = binding.loginBtnEnter
+        btnEnter.setOnClickListener {
+            btnEnter.showProgress(true)
 
 
+            email.error = "E-mail inválido"
+            password.error = "Senha inválida"
 
-  }
+            Handler(Looper.getMainLooper()).postDelayed({
+                btnEnter.showProgress(false)
+            }, 2000)
+        }
+
+    }
+
+    private val watcher = object: TextWatcher{
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            binding.loginBtnEnter.isEnabled = p0.toString().isNotEmpty()
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+        }
+
+    }
 }
