2803a6cee00e2be3f69496355d558b5417788cf3
Beto_Dev1# <gilbertocalixto2002@gmail.com>
2024-09-21 17:21:53 -0300

    Animação de Scrolling com NestedScrollView e CoordinatorLayout

--- app/src/main/java/co/tiagoaguiar/course/instagram/view/view/main/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/view/view/main/MainActivity.kt
@@ -1,20 +1,36 @@
 package co.tiagoaguiar.course.instagram.view.view.main
 
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.marginBottom
+import androidx.core.view.marginTop
+import androidx.core.view.setPadding
 import co.tiagoaguiar.course.instagram.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+//            // Obtém a referência do BottomNavigationView
+//            val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav_profile)
+//
+//            // Evite definir padding diretamente no BottomNavigationView que pode aumentar sua altura
+//            val layoutParams = bottomNavView.layoutParams as ViewGroup.MarginLayoutParams
+//
+//            // Aplica a margem inferior para compensar a barra de navegação do sistema sem alterar o padding
+//            layoutParams.bottomMargin = systemBars.bottom
+//            bottomNavView.layoutParams = layoutParams
+
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right,0)
             insets
         }
     }
--- app/src/main/java/co/tiagoaguiar/course/instagram/view/view/profile/FragmentProfile.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/view/view/profile/FragmentProfile.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.course.instagram.R
 import co.tiagoaguiar.course.instagram.databinding.ActivityLoginBinding
 import co.tiagoaguiar.course.instagram.databinding.FragmentProfileBinding
 
@@ -19,5 +23,41 @@ class FragmentProfile: Fragment() {
     ): View {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val rv = binding.rcvProfile
+        rv.layoutManager = GridLayoutManager(requireContext(), 3)
+        rv.adapter = PostAdapter()
+
+
+    }
+
+    private class PostAdapter(): RecyclerView.Adapter<PostAdapter.PostViewHolder>(){
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): PostAdapter.PostViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_profile_grid, parent, false)
+            return PostViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: PostAdapter.PostViewHolder, position: Int) {
+            holder.bind(R.drawable.ic_insta_add)
+        }
+
+        override fun getItemCount(): Int {
+            return 30
+        }
+
+        inner class PostViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+            fun bind(image: Int) {
+                itemView.findViewById<ImageView>(R.id.item_rcv_grid_profile).setImageResource(image)
+            }
+        }
+
+
     }
 }
