ba5228fa65906476d13a8f2b1be2107d9a65a5be
Beto_Dev1# <gilbertocalixto2002@gmail.com>
2024-09-21 17:30:53 -0300

    Barra de Status Programaticamente

--- app/src/main/java/co/tiagoaguiar/course/instagram/view/view/main/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/view/view/main/MainActivity.kt
@@ -1,9 +1,13 @@
 package co.tiagoaguiar.course.instagram.view.view.main
 
+import android.os.Build
 import android.os.Bundle
 import android.view.ViewGroup
+import android.view.WindowInsetsController
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.marginBottom
@@ -13,22 +17,18 @@ import co.tiagoaguiar.course.instagram.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        window.insetsController?.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.gray)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-
-//            // Obtém a referência do BottomNavigationView
-//            val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav_profile)
-//
-//            // Evite definir padding diretamente no BottomNavigationView que pode aumentar sua altura
-//            val layoutParams = bottomNavView.layoutParams as ViewGroup.MarginLayoutParams
-//
-//            // Aplica a margem inferior para compensar a barra de navegação do sistema sem alterar o padding
-//            layoutParams.bottomMargin = systemBars.bottom
-//            bottomNavView.layoutParams = layoutParams
 
             v.setPadding(systemBars.left, systemBars.top, systemBars.right,0)
             insets
