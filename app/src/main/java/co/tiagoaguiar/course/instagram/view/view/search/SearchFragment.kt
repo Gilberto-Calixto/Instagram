ad3d2f996d2941971e4dd65b1b1d7478e495a4cb
Beto_Dev1# <gilbertocalixto2002@gmail.com>
2024-09-21 23:51:34 -0300

    Tela de Pesquisa de Usuário

---
+++ app/src/main/java/co/tiagoaguiar/course/instagram/view/view/search/SearchFragment.kt
@@ -0,0 +1,53 @@
+package co.tiagoaguiar.course.instagram.view.view.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.course.instagram.R
+
+class SearchFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        //Config rcv
+        val rcv = view.findViewById<RecyclerView>(R.id.rv_search_users)
+        rcv.layoutManager = LinearLayoutManager(requireContext())
+        rcv.adapter = HomeAdapter()
+    }
+
+    private class HomeAdapter: RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): HomeAdapter.HomeViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_search_users, parent, false)
+            return HomeViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: HomeAdapter.HomeViewHolder, position: Int) {
+            holder.bind(R.drawable.ic_insta_add)
+        }
+
+        override fun getItemCount() = 30
+
+        inner class HomeViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+            fun bind(image: Int) {
+
+            }
+        }
+
+    }
+}
