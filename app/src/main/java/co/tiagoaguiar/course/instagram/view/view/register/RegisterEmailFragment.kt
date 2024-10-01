6cc4b8f474d17f76e525db808b5b1a4ce87316b0
Beto_Dev1# <gilbertocalixto2002@gmail.com>
2024-10-01 00:17:51 -0300

    EmailView : Validação de Email

--- app/src/main/java/co/tiagoaguiar/course/instagram/view/view/register/RegisterEmailFragment.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/view/view/register/RegisterEmailFragment.kt
@@ -1,21 +1,58 @@
 package co.tiagoaguiar.course.instagram.view.view.register
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import co.tiagoaguiar.course.instagram.R
 import co.tiagoaguiar.course.instagram.databinding.FragmentRegisterEmailBinding
+import co.tiagoaguiar.course.instagram.view.cases.RegisterEmail
+import co.tiagoaguiar.course.instagram.view.common.TxtWatch
 
-class RegisterEmailFragment: Fragment(R.layout.fragment_register_email) {
+class RegisterEmailFragment: Fragment(R.layout.fragment_register_email), RegisterEmail.View {
 
-    private lateinit var binding: FragmentRegisterEmailBinding
+    override lateinit var presenter: RegisterEmail.Presenter
+    private var binding: FragmentRegisterEmailBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding = FragmentRegisterEmailBinding.inflate(layoutInflater)
+        binding = FragmentRegisterEmailBinding.bind(view)
+        binding?.let{
+            with(it) {
+                texteLogin.setOnClickListener {
+                    activity?.finish()
+                }
 
-        binding.texteLogin.setOnClickListener {
+                val emailRegisterUser = binding?.registerEditEmail
 
+                btnRegisterEmail.setOnClickListener {
+                    presenter.create(emailRegisterUser?.text.toString())
+                }
+
+                emailRegisterUser?.addTextChangedListener(watcher)
+                emailRegisterUser?.addTextChangedListener(TxtWatch{
+                    displayEmailFailure(null)
+                })
+            }
         }
+
     }
+
+    override fun displayEmailFailure(message: Int?) {
+
+    }
+
+    private val watcher = TxtWatch {
+        binding?.registerEditEmail?.isEnabled = binding?.registerEditEmail?.text.toString().isNotEmpty()
+//                && binding.loginTextPassword.text.toString().isNotEmpty()
+    }
+
+    override fun onDestroy() {
+        //presenter.onDestroy()
+        binding = null
+        super.onDestroy()
+
+    }
+
 
 }
